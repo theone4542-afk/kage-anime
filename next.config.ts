d1c269b1e8b233b170128574774630a95eb054d5
theone4542-afk <theone4542@gmail.com>
2026-05-16 22:45:44 +0530

    fix: Added Webtorrent

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@consumet/extensions", "got-scraping"],
+  serverExternalPackages: ["@consumet/extensions", "got-scraping", "webtorrent"],
   images: {
     remotePatterns: [
       {
